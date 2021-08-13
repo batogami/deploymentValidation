a6e9660ac54cce4fbf5fc51601fef8194da80741
mb-ecolytiq <maximilian.bartels@ecolytiq.com>
2021-08-13 13:13:29 +0200

    upload html parser tests

--- src/test/java/HtmlParserTest.java
+++ src/test/java/HtmlParserTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Base64;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HtmlParserTest {
@@ -7,20 +9,46 @@ class HtmlParserTest {
     @Test
     void
     externalServiceToStringParser() {
+       String test =  HtmlParser.externalServiceToStringParser("TestName", "TestURl");
+       String base64Url = Base64.getUrlEncoder().encodeToString("TestURl".getBytes());
+       assertEquals("TestName/x/"+base64Url,test);
     }
 
     @Test
     void
     externalServiceToCiidParser() {
+        Ciid test =  HtmlParser.externalServiceToCiidParser("TestName", "TestURl");
+        String base64Url = Base64.getUrlEncoder().encodeToString("TestURl".getBytes());
+        assertEquals(test.miid.sn, "TestName");
+        assertEquals(test.miid.va, base64Url);
+        assertEquals(test.miid.vn, "x");
     }
 
     @Test
     void
-    externalServiceToCiidParserFail() {
+    externalServiceToCiidParserFailNameEmpty() {
+        String test =  HtmlParser.externalServiceToStringParser("", "TestURl");
+        assertNull(test);
     }
 
     @Test
     void
-    externalServiceToStringParserFail() {
+    externalServiceToCiidParserFailURLEmpty() {
+        String test =  HtmlParser.externalServiceToStringParser("TestName", "");
+        assertNull(test);
+    }
+
+    @Test
+    void
+    externalServiceToStringParserFailNameEmpty() {
+        String test =  HtmlParser.externalServiceToStringParser("", "TestURl");
+        assertNull(test);
+    }
+
+    @Test
+    void
+    externalServiceToStringParserFailURLEmpty() {
+        Ciid test =  HtmlParser.externalServiceToCiidParser("TestName", "");
+        assertNull(test);
     }
 }
